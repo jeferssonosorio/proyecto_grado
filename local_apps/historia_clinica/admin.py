b4bb6fdb75c746014b61beab07686e5abde9f4da
jefersson osorio <jefersson.osorio@ucp.edu.co>
2021-11-22 14:43:18 -0500

    -Se completa modelo 'paciente' -Se corren migraciones de 'paciente' -Se crea modelo 'patologia' -Se corren migraciones de 'patologia'

--- local_apps/historia_clinica/admin.py
+++ local_apps/historia_clinica/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from local_apps.historia_clinica.models.paciente import Paciente
+from local_apps.historia_clinica.models.patologia import Patologia
+
 
 # Register your models here.
+admin.site.register([Paciente, Patologia])
--- local_apps/historia_clinica/models/patologia.py
+++ local_apps/historia_clinica/models/patologia.py
@@ -2,4 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Patologia(models.Model):
-    nombre = models.CharField(max_length=50)
+    nombre = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.nombre
