5d4a53dee0cc0f5ba097f707e66afd199867981d
Juan Bahos <juan.bahos@ucp.edu.co>
2021-11-22 11:29:34 -0500

    Se crean realciones entre modelos evoluciones-paciente, evoluciones-procedimientos

--- local_apps/historia_clinica/models/evoluciones.py
+++ local_apps/historia_clinica/models/evoluciones.py
@@ -1,6 +1,10 @@
 from django.db import models
+from local_apps.historia_clinica.models.paciente import Paciente
+from local_apps.historia_clinica.models.procedimiento import Procedimiento
 
 # Create your models here.
 class Evoluciones(models.Model):
     descripcion = models.TextField()
     fecha = models.DateTimeField()
+    paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE())
+    procedimiento = models.ManyToManyField(Procedimiento)
