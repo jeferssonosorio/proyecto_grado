89078d673f1d1eb54aaca9df3e410dbc3272b167
Juan Bahos <juan.bahos@ucp.edu.co>
2021-11-22 13:46:58 -0500

    Se modifica el modelo procedimiento

--- local_apps/historia_clinica/models/procedimiento.py
+++ local_apps/historia_clinica/models/procedimiento.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Procedimiento(models.Model):
-    nombre = models.CharField(max_length=50)
+    nombre = models.CharField(max_length=50, unique=True)
 
     def __str__(self):
         return self.nombre
