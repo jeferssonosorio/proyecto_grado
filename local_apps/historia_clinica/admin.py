dfe7776426ad05523b2d0d669b45aa70b2205d00
jefersson osorio <jefersson.osorio@ucp.edu.co>
2021-11-22 14:50:52 -0500

    Se eliminan modificaciones en admin

--- local_apps/historia_clinica/admin.py
+++ local_apps/historia_clinica/admin.py
@@ -1,7 +1,3 @@
 from django.contrib import admin
-from local_apps.historia_clinica.models.paciente import Paciente
-from local_apps.historia_clinica.models.patologia import Patologia
-
 
 # Register your models here.
-admin.site.register([Paciente, Patologia])
