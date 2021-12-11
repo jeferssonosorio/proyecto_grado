59542a0196de2960980bb721a4969e017533f626
Juan Bahos <juan.bahos@ucp.edu.co>
2021-12-11 18:07:27 -0500

    Se realiza cambio en el modelo evoluciones y se corren migraciones

--- local_apps/historia_clinica/models/evoluciones.py
+++ local_apps/historia_clinica/models/evoluciones.py
@@ -2,6 +2,7 @@ import datetime
 from django.db import models
 from local_apps.historia_clinica.models.paciente import Paciente
 from local_apps.historia_clinica.models.procedimiento import Procedimiento
+from local_apps.historia_clinica.models.usuario import Usuario
 
 
 # Create your models here.
@@ -10,6 +11,7 @@ class Evoluciones(models.Model):
     fecha = models.DateField(default=datetime.date.today)
     paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE)
     procedimiento = models.ManyToManyField(Procedimiento)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
 
     def __str__(self):
         return "Paciente: {}, Fecha: {}".format(self.paciente, self.fecha)
