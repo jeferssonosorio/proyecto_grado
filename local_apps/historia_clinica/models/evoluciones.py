046fbd83e82875a3f958c9c00c7bbbe29c6e38e3
jefersson osorio <jefersson.osorio@ucp.edu.co>
2021-12-11 23:42:46 -0500

    -Se instala librería django_extensions y se agrega a requirements.txt -Se modifica modelo de Usuarios para usar el por defecto de Django -Se modifican todos los modelos para que hereden de TimeStampedModel y tengan columnas'creado' y 'modificado0 -Se crean modelos afecciones y diagnostico -Se crean migraciones -Se agregan modelos a admin-django

--- local_apps/historia_clinica/models/evoluciones.py
+++ local_apps/historia_clinica/models/evoluciones.py
@@ -1,17 +1,24 @@
 import datetime
 from django.db import models
-from local_apps.historia_clinica.models.paciente import Paciente
+from django_extensions.db.models import TimeStampedModel
 from local_apps.historia_clinica.models.procedimiento import Procedimiento
 from local_apps.historia_clinica.models.usuario import Usuario
-
+from local_apps.historia_clinica.models.diagnostico import Diagnostico
 
 # Create your models here.
-class Evoluciones(models.Model):
+class Evoluciones(TimeStampedModel):
     descripcion = models.TextField()
     fecha = models.DateField(default=datetime.date.today)
-    paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE)
     procedimiento = models.ManyToManyField(Procedimiento)
     usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    diagnostico = models.ForeignKey(Diagnostico, on_delete=models.CASCADE)
 
     def __str__(self):
-        return "Paciente: {}, Fecha: {}".format(self.paciente, self.fecha)
+        procedimiento = ", ".join(
+            str(procedimiento) for procedimiento in self.procedimiento.all()
+        )
+        return "Paciente: {} | Fecha Diagnostico: {} | Procedimientos: {}".format(
+            self.diagnostico.paciente.documento,
+            self.diagnostico.fecha,
+            procedimiento,
+        )
--- local_apps/historia_clinica/models/usuario.py
+++ local_apps/historia_clinica/models/usuario.py
@@ -1,26 +1,34 @@
+import uuid as uuid
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django_extensions.db.models import TimeStampedModel
+from local_apps.historia_clinica.managers import UsuarioManager
 from local_apps.historia_clinica.models.tipoDocIdentificacion import (
     TipoDocIdentificacion,
 )
 
 
 # Create your models here.
-class Usuario(models.Model):
-    # El campo usuario hace referencia al Usuario y Contraseña de Logueo
-    usuario = models.CharField(max_length=30)
-    # contrasenia = models.CharField(max_length=30)
-    correo = models.EmailField(max_length=30)
-    nombre = models.CharField(max_length=20)
-    apellido_1 = models.CharField(max_length=30)
-    apellido_2 = models.CharField(max_length=30, blank=True, null=True)
+class Usuario(AbstractUser, TimeStampedModel):
+    """
+    Representa un Usuario en el sistema
+    """
+
+    uuid = models.UUIDField(
+        db_index=True, default=uuid.uuid4, editable=False, unique=True
+    )
+    email = models.EmailField(unique=True)
     tipo_doc_identificacion = models.ForeignKey(
         TipoDocIdentificacion, on_delete=models.CASCADE
     )
     doc_identificacion = models.CharField(max_length=20, unique=True)
+    objects = UsuarioManager()
+
+    class Meta:
+        app_label = "historia_clinica"
+        verbose_name = "usuario"
+        verbose_name_plural = "usuarios"
+        default_permissions = ()
 
     def __str__(self):
-        return "{} {} {}".format(
-            self.nombre,
-            self.apellido_1,
-            self.apellido_2 if self.apellido_2 is not None else "",
-        )
+        return "{} {}".format(self.first_name, self.last_name)
